7b6e02fc639c61003a4d89cbee5530173e233fbb
nishantkadam-dev <nishant.k@incubyte.co>
2026-05-26 16:14:25 +0530

    green: support custom single-character delimiter in header

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,7 +8,16 @@ class StringCalculator
     return 0 if numbers.empty? || numbers.nil?
     # default delimiters: comma and newline
     delimiters = [',', "\n"]
-    nums = numbers.split(Regexp.union(delimiters)).map(&:to_i)
+    nums_str = numbers
+    
+    # check for custom delimiter in header
+    if numbers.start_with?('//')
+      header, nums_str = numbers.split("\n", 2)
+      custom_delimiter = header[2..-1] # get the part after '//'
+      delimiters << custom_delimiter
+    end
+
+    nums = nums_str.split(Regexp.union(delimiters)).map(&:to_i)
     nums.sum
   end
 end
