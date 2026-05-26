ea294232b109455089bb7d4cd3b9fca5e3106e59
nishantkadam-dev <nishant.k@incubyte.co>
2026-05-26 15:42:07 +0530

    refactor: remove comment from method body

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,7 +6,6 @@ class StringCalculator
   def add(numbers)
     @count += 1
     return 0 if numbers.empty? || numbers.nil?
-    # Further behavior implemented in later steps
     numbers.to_i
   end
 end
