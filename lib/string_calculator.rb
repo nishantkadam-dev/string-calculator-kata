2e9eaac02872d90a060ba184aa834cc421795be4
nishantkadam-dev <nishant.k@incubyte.co>
2026-05-26 15:37:42 +0530

    green: implement empty-string behavior

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,12 @@
 class StringCalculator
   def initialize
+    @count = 0
   end
 
   def add(numbers)
-    raise NotImplementedError
+    @count += 1
+    return 0 if numbers.empty? || numbers.nil?
+    # Further behavior implemented in later steps
+    numbers.to_i
   end
 end
