6d3753da2e3e9294b263262fe93c2949b7872402
nishantkadam-dev <nishant.k@incubyte.co>
2026-05-26 16:04:44 +0530

    green: support newline as delimiter and unknown amount of numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,8 +6,8 @@ class StringCalculator
   def add(numbers)
     @count += 1
     return 0 if numbers.empty? || numbers.nil?
-    # default delimiters: comma
-    delimiters = [',']
+    # default delimiters: comma and newline
+    delimiters = [',', "\n"]
     numbers.split(Regexp.union(delimiters)).map(&:to_i).sum
   end
 end
